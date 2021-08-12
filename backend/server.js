e734c5a9e27570bc3174c8678eab98d9c1f11af4
Gopal Krishan <gopalkbks@Gopals-MacBook-Pro.local>
2021-08-11 22:44:51 -0400

    all good

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,24 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import products from './products.js';
+import colors from 'colors';
+import products from './data/products.js';
+import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
 
 const app = express();
 // const products = require('./products');
 
 dotenv.config();
+connectDB();
 
 app.get('/', (req, res) => {
 	res.send('Homepage.....');
 });
 
-app.get('/api/products', (req, res) => {
-	res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-	const product = products.find((p) => p._id === req.params.id);
-	res.json(product);
-});
+app.use('/api/products', productRoutes);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-	console.log(`server running on ${PORT}`);
+	console.log(`server running on ${PORT}`.yellow.underline.bold);
 });
